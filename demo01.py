c32199138a4b47c9fb0e98e34df8a5a7c9ce48fd
xieyuna <976994709@qq.com>
2020-11-08 12:36:23 +0800

    第一次向github推送代码

---
+++ demo01.py
@@ -0,0 +1,30 @@
+
+#数据类型的转换
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))   #元组
+# print(type([]))   #数组
+# print(type({}))  #字典
+
+
+"""
+print("你好")
+a = input("请输入：")   #输入和赋值
+b = input("请输入：")
+print("input输入的内容是：",a+b)  #输出的是字符串
+
+
+
+print("你好")
+a = float(input("请输入："))   #输入和赋值
+b = float(input("请输入："))
+print("input输入的内容是：",a+b)    #结果
+"""
+
+
+#len()函数：计算字符串的长度
+a = input("请输入：")   #输入和赋值
+b = input("请输入：")
+print("input输入的内容是：",len(a+b))  #输出的是字符串
